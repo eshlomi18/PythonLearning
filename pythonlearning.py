7a16ecbc77fb00b0e56e355348c3344ec1959eb4
Evyatar Shlomi <eshlomi15@gmail.com>
2022-08-23 13:38:39 +0300

    added random_number function

--- pythonlearning.py
+++ pythonlearning.py
@@ -67,3 +67,20 @@ def online_count(status):
         if value == "online":
             counter += 1
     return counter
+
+
+"""
+Randomness
+Define a function, random_number, that takes no parameters.
+The function must generate a random integer between 1 and 100, both inclusive, and return it.
+
+Calling the function multiple times should (usually) return different numbers.
+
+For example, calling random_number() some times might first return 42, then 63, then 1.
+"""
+
+
+def random_number():
+    import random
+    number = random.randint(1, 100)
+    return int(number)
