3bc44203a43684ced3483209bd7871c1747d7346
Evyatar Shlomi <eshlomi15@gmail.com>
2022-08-23 12:35:03 +0300

    added middle letter function

--- pythonlearning.py
+++ pythonlearning.py
@@ -24,3 +24,18 @@ def capital_indexes2(string):
         if letter.isupper():
             result.append(i)
     return result
+
+
+"""
+Middle letter
+Write a function named mid that takes a string as its parameter. Your function should extract and return the middle letter. If there is no middle letter, your function should return the empty string.
+
+For example, mid("abc") should return "b" and mid("aaaa") should return "".
+"""
+
+
+def mid(word):
+    if len(word) % 2 == 1:
+        return word[len(word) // 2]
+    else:
+        return ""
