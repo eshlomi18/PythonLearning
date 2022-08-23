45ed9786f8d01d742debc4e3169d00154f04a83b
Evyatar Shlomi <eshlomi15@gmail.com>
2022-08-23 14:23:40 +0300

    added only_ints function

--- pythonlearning.py
+++ pythonlearning.py
@@ -84,3 +84,19 @@ def random_number():
     import random
     number = random.randint(1, 100)
     return int(number)
+
+
+"""
+Type check
+Write a function named only_ints that takes two parameters.
+Your function should return True if both parameters are integers, and False otherwise.
+
+For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+"""
+
+
+def only_ints(variable1, variable2):
+    return type(variable1) == int and type(variable2) == int
+
+
+
