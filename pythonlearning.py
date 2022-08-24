5b6c0dc2b6c945d7988830bddaf8e92f4f1a1ae0
Evyatar Shlomi <eshlomi15@gmail.com>
2022-08-24 10:57:56 +0300

    added count function

--- pythonlearning.py
+++ pythonlearning.py
@@ -152,3 +152,26 @@ def remove_dots(word):
     for element in range(0, len(result)):
         help += result[element]
     return help
+
+
+"""
+Counting syllables
+Define a function named count that takes a single parameter.
+The parameter is a string. The string will contain a single word divided into syllables by hyphens, such as these:
+
+"ho-tel"
+"cat"
+"met-a-phor"
+"ter-min-a-tor"
+Your function should count the number of syllables and return it.
+
+For example, the call count("ho-tel") should return 2.
+"""
+
+
+def count(word):
+    counter = 1
+    for element in word:
+        if element == "-":
+            counter += 1
+    return counter
